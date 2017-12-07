7ca9eb99d30dad08081f6f38e5e64c160ec8b85f
Knitschi <knitschi@freenet.de>
2017-12-07 10:19:48 +0100

    add python init file

--- addJenkinsJob.py
+++ addJenkinsJob.py
@@ -3,8 +3,8 @@
 import shutil
 import os
 import io
+import sys
 
-import CppCodeBaseMachines.setupDockerContainer
 
 # locations
 _scriptDir = os.path.dirname(os.path.realpath(__file__))
@@ -20,7 +20,7 @@ def addCppCodeBaseJob( jobName, repositoryAddress, jenkinsDirectory):
     """
     # remove the existing job
     jobDir = _getJobDir(jenkinsDirectory, jobName)
-    setupDockerContainer.clearDirectory(jobDir)
+    _clearDirectory(jobDir)
 
     # add a new config.xml file
     _configureJobConifgFile(jobDir, jobName, repositoryAddress)
@@ -33,7 +33,7 @@ def addCustomJob( jobName, configFile, jenkinsDirectory):
     """
     # remove the existing job
     jobDir = _getJobDir(jenkinsDirectory, jobName)
-    setupDockerContainer.clearDirectory(jobDir)
+    _clearDirectory(jobDir)
 
     # copy the config file
     shutil.copyfile( configFile, jobDir + '/config.xml')
@@ -47,12 +47,39 @@ def _configureJobConifgFile( destDir, jobName, repositoryAddress ):
     """
     Fills in the blanks in the config file and copies it to the given job directory.
     """
-    setupDockerContainer.configureFile(_templateFile,destDir + '/config.xml', { 
+    _configureFile(_templateFile,destDir + '/config.xml', { 
         '$JOB_NAME' : jobName,
         '$JENKINSFILE_VERSION' : _jenkinsfileVersion,
         '$BUILD_REPOSITORY' : repositoryAddress
     } )
 
 
+def _clearDirectory(directory):
+    """
+    This functions deletes the given directory and all its content and recreates it.
+
+    The function is duplicated in the CppCodeBaseMachines setupDockerContainer.py file.
+    """
+    if(os.path.isdir(directory)):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
 
 
+def _configureFile( sourceFile, destFile, replacementDictionary ):
+    """
+    Searches in sourceFile for the keys in replacementDictionary, replaces them
+    with the values and writes the result to destFile.
+
+    The function is duplicated in the CppCodeBaseMachines setupDockerContainer.py file.
+    """
+    # Open target file
+    configFile = io.open( destFile, 'w')
+
+    # Read the lines from the template, substitute the values, and write to the new config file
+    for line in io.open( sourceFile, 'r'):
+        for key, value in replacementDictionary.items():
+            line = line.replace( key, value)
+        configFile.write(line)
+
+    # Close target file
+    configFile.close()
