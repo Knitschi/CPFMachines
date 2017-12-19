e8a142ae6d72a2612f4e3f5eba0d1fd5ca231513
Knitschi <knitschi@freenet.de>
2017-12-19 10:46:38 +0100

    fixed import

--- DockerImages/setupDockerContainer.py
+++ DockerImages/setupDockerContainer.py
@@ -19,7 +19,7 @@ import getpass
 import time
 import requests
 
-from . import CppCodeBaseMachines_version
+import CppCodeBaseMachines_version
 
 _SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
