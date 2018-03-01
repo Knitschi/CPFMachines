efba05b57c93753e177ab36c8f4245fa6828fd7d
Knitschi <knitschi@freenet.de>
2018-03-01 09:31:48 +0100

    move job config to normal config

--- config_data_tests.py
+++ config_data_tests.py
@@ -98,6 +98,58 @@ class TestConfigData(unittest.TestCase):
         self.assertEqual( sut.jenkins_config.approved_system_commands[0], 'ssh bla blub' )
         self.assertEqual( sut.jenkins_config.approved_script_signatures[0] , '<script signature from my MyCustomJob jenkinsfile>')
 
+        # JOB 1
+        self.assertEqual( sut.job_configs[0].job_name, 'MyCPFProject1')
+        self.assertEqual( sut.job_configs[0].repository, 'ssh://fritz@mastermachine:/home/fritz/repositories/MyCPFProject1.git')
+
+        self.assertEqual( sut.job_configs[0].webserver_config.machine_id, 'MyMaster')
+        self.assertEqual( sut.job_configs[0].webserver_config.host_html_share_dir, PurePosixPath('/home/fritz/mycpfproject1_html_share'))
+        self.assertEqual( sut.job_configs[0].webserver_config.container_ssh_port, 25)
+        self.assertEqual( sut.job_configs[0].webserver_config.container_web_port, 80)
+
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.container_name, 'cpf-web-server-0')
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.container_user, 'root')
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.container_image_name, 'cpf-web-server-image')
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.published_ports, {80:80, 25:22})
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.host_volumes, {PurePosixPath('/home/fritz/mycpfproject1_html_share') : PurePosixPath('/var/www/html')})
+        self.assertEqual( sut.job_configs[0].webserver_config.container_conf.envvar_definitions, [])
+
+        # JOB 2
+        self.assertEqual( sut.job_configs[1].job_name, 'MyCPFProject2')
+        self.assertEqual( sut.job_configs[1].repository, 'ssh://fritz@mastermachine:/home/fritz/repositories/MyCPFProject2.git')
+
+        self.assertEqual( sut.job_configs[1].webserver_config.machine_id, 'MyMaster')
+        self.assertEqual( sut.job_configs[1].webserver_config.host_html_share_dir, PurePosixPath('/home/fritz/mycpfproject2_html_share'))
+        self.assertEqual( sut.job_configs[1].webserver_config.container_ssh_port, 26)
+        self.assertEqual( sut.job_configs[1].webserver_config.container_web_port, 81)
+
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.container_name, 'cpf-web-server-1')
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.container_user, 'root')
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.container_image_name, 'cpf-web-server-image')
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.published_ports, {81:80, 26:22})
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.host_volumes, {PurePosixPath('/home/fritz/mycpfproject2_html_share') : PurePosixPath('/var/www/html')})
+        self.assertEqual( sut.job_configs[1].webserver_config.container_conf.envvar_definitions, [])
+
+
+    def test_validation_checks_that_all_hosts_are_in_use(self):
+        """
+        If a host is not referenced, it is probably an error in the config file.
+        """
+        # setup
+        config_dict = get_example_config_dict()
+        # add a host that is not used
+        not_used_host_dict = {
+                KEY_MACHINE_ID : 'MyMaster2',
+                KEY_MACHINE_NAME : 'lhost4',
+                KEY_USER : 'fritz',
+                KEY_PASSWORD : '1234password',
+                KEY_OSTYPE : 'Linux'
+            }
+        config_dict[KEY_LOGIN_DATA].append(not_used_host_dict)
+
+        # execute
+        self.assertRaises(Exception, ConfigData, config_dict)
+
 
     def test_validation_checks_version(self):
         """
@@ -133,6 +185,15 @@ class TestConfigData(unittest.TestCase):
 
         # execute
         self.assertRaises(Exception, ConfigData, config_dict)
+
+        # setup
+        self.assertTrue(False) # check that changing the machine of a webserver to windows will also throw exception
+        #config_dict = get_example_config_dict()
+        #config_dict[config_data.KEY_JENKINS_CONFIG][KEY_CPF_JOBS][0][KEY_WEBSERVER_CONFIG][config_data.KEY_MACHINE_ID] = 'MyWindowsSlave'
+
+        # execute
+        #self.assertRaises(Exception, CPFJobConfigs, config_dict)
+
 
 
     def test_validation_checks_that_host_ids_are_unique(self):
