f0ac48db8c6e90c37d7d19a8cf5e29bae94e2da6
Knitschi <knitschi@freenet.de>
2018-03-09 11:38:51 +0100

    try debug html copy

--- JenkinsPipelineScript.groovy
+++ JenkinsPipelineScript.groovy
@@ -345,7 +345,6 @@ def addUpdateWebPageStage(repository, cpfConfigs, commitID)
                 // merge the new html content into the old html content
                 // sh "ls -l \$PWD/${CHECKOUT_FOLDER}/Sources/cmake/Scripts"
                 sh "cmake -DFRESH_HTML_DIR=\"${newHtmlContentDir}\" -DEXISTING_HTML_DIR=\"${oldHtmlContentDir}\" -DROOT_DIR=\"\$PWD/${CHECKOUT_FOLDER}\" -P \"\$PWD/${CHECKOUT_FOLDER}/Sources/${CPFCMAKE_DIR}/Scripts/updateExistingWebPage.cmake\""
-                sh "tree html"
 
                 // copy the merge result back to the server
                 sh "ssh -p ${port} ${web_host} \"rm -rf ${projectHtmlDirOnWebserver}\""
