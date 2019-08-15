2f6a29d6e961dee3b81f05ec4a6651a1938ec1f4
Knitschi <knitschi@freenet.de>
2019-08-15 21:02:29 +0200

    Fixes the broken @BUILD_RESULT_REPOSITORY_WEB_SERVER@ buildjob paramter.

--- setup.py
+++ setup.py
@@ -803,6 +803,8 @@ class MachinesController:
             info = self.connections.get_connection(cpf_job_config.webserver_config.machine_id).info
             port = cpf_job_config.webserver_config.container_ssh_port
             jobConfigVariableMap['@BUILD_RESULT_REPOSITORY_WEB_SERVER@'] = 'ssh://jenkins@{0}:{1}:/home/jenkins/WebContentRepository'.format(info.host_name, port)
+        else:
+            jobConfigVariableMap['@BUILD_RESULT_REPOSITORY_WEB_SERVER@'] = ''
 
         configure_file(_CPF_JOB_TEMPLATE_FILE, created_config_file, jobConfigVariableMap)
 
